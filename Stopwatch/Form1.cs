91a65890bb2e6efc460d948cf61773724e4eed83
vfedotova418-png <vfedotova418@gmail.com>
2025-10-31 08:48:51 +0300

    refactoring done

--- Stopwatch/Form1.cs
+++ Stopwatch/Form1.cs
@@ -3,9 +3,10 @@ using System.Windows.Forms;
 
 namespace Stopwatch {
     public partial class Form1 : Form {
-        private int hours, minutes, seconds, milliseconds;
+        private readonly StopwatchModel stopwatch;
         public Form1() {
             InitializeComponent();
+            stopwatch = new StopwatchModel(15);
             timer1.Interval = 10;
         }
 
@@ -15,28 +16,16 @@ namespace Stopwatch {
 
         private void resetBTN_Click(object sender, EventArgs e) {
             timer1.Stop();
-            hours = minutes = seconds = milliseconds = 0;
+            stopwatch.Reset();
             UpdateLabel();
         }
 
         private void UpdateLabel() {
-            label1.Text = $"{hours:00}:{minutes:00}:{seconds:00}:{milliseconds:000}";
+            label1.Text = stopwatch.GetFormattedTime();
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            milliseconds += 15;
-            if (milliseconds >= 1000) {
-                milliseconds = 0;
-                seconds++;
-            }
-            if (seconds >= 60) {
-                seconds = 0;
-                minutes++;
-            }
-            if (minutes >= 60) {
-                minutes = 0;
-                hours++;
-            }
+            stopwatch.Tick();
             UpdateLabel();
         }
     }
---
+++ Stopwatch/StopwatchModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch {
+    public class StopwatchModel {
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+        public int Milliseconds { get; private set; }
+        private readonly int tickStep;
+
+        public StopwatchModel(int tickStep = 15) {
+            this.tickStep = tickStep;
+        }
+
+        public void Tick() {
+            Milliseconds += tickStep;
+            if (Milliseconds >= 1000) {
+                Milliseconds -= 1000;
+                Seconds++;
+            }
+            if (Seconds >= 60) {
+                Seconds -= 60;
+                Minutes++;
+            }
+            if (Minutes >= 60) {
+                Minutes -= 60;
+                Hours++;
+            }
+        }
+
+        public void Reset() {
+            Hours = Minutes = Seconds = Milliseconds = 0;
+        }
+
+        public string GetFormattedTime() {
+            return $"{Hours:00}:{Minutes:00}:{Seconds:00}:{Milliseconds / 10:00}";
+        }
+    }
+}
